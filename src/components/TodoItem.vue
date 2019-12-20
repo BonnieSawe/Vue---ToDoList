1cf613c8d0f6e7241209ff8770bb6349b2f6e60f
Dave Njoroge <davenjoroge10@gmail.com>
2019-12-20 10:56:12 +0300

    added feather icons, minor additions

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -8,16 +8,40 @@
         :id="id"
         :checked="todo.completed"
       />
-      <p class="todo-text m-0">{{todo.title}}</p>
+
+      <check-square-icon v-if="todo.completed"></check-square-icon>
+      <square-icon v-else></square-icon>
+      <p class="todo-text m-0 pl-3">{{todo.title}}</p>
     </label>
-    <button class="ml-auto btn btn-outline-danger btn-sm" @click="$emit('del-todo', todo.id)">Delete</button>
+    <button class="ml-auto btn text-danger delete-btn" @click="$emit('del-todo', todo.id)">
+      <minus-square-icon></minus-square-icon>
+    </button>
   </div>
 </template>
 
 <script>
+/* 
+    // TIP 
+    
+    added icons from a package called vue-feather-icons, which is a Vue
+    usable package for Feather Icons.
+
+    note the components imported, registered and used in the code
+  
+  */
+import {
+  CheckSquareIcon,
+  SquareIcon,
+  MinusSquareIcon
+} from "vue-feather-icons";
 export default {
   name: "TodoItem",
   props: ["todo", "id"],
+  components: {
+    CheckSquareIcon,
+    SquareIcon,
+    MinusSquareIcon
+  },
   methods: {
     markComplete() {
       this.todo.completed = !this.todo.completed;
@@ -29,7 +53,7 @@ export default {
 };
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 /**
 
 // TIP
@@ -67,5 +91,11 @@ design
 }
 
 */
+
+.delete-btn {
+  &:hover {
+    background-color: transparent;
+  }
+}
 </style>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -6,12 +6,12 @@
 </template>
 
 <script>
-import Todos from '../components/Todos';
-import AddTodo from '../components/AddTodo';
-import axios from 'axios';
+import Todos from "../components/Todos";
+import AddTodo from "../components/AddTodo";
+import axios from "axios";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     Todos,
     AddTodo
@@ -19,55 +19,58 @@ export default {
   data() {
     return {
       todos: []
-    }
+    };
   },
   methods: {
     deleteTodo(id) {
-      axios.delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
-        .then(res => this.todos = this.todos.filter(todo => todo.id !== id))
+      axios
+        .delete(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(res => (this.todos = this.todos.filter(todo => todo.id !== id)))
         .catch(err => console.log(err));
     },
     addTodo(newTodo) {
       const { title, completed } = newTodo;
 
-      axios.post('https://jsonplaceholder.typicode.com/todos', {
-        title,
-        completed
-      })
-        .then(res => this.todos = [...this.todos, res.data])
+      axios
+        .post("https://jsonplaceholder.typicode.com/todos", {
+          title,
+          completed
+        })
+        .then(res => (this.todos = [...this.todos, res.data]))
         .catch(err => console.log(err));
     }
   },
   created() {
-    axios.get('https://jsonplaceholder.typicode.com/todos?_limit=5')
-      .then(res => this.todos = res.data)
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+      .then(res => (this.todos = res.data))
       .catch(err => console.log(err));
   }
-}
+};
 </script>
 
 <style>
-  * {
-    box-sizing: border-box;
-    margin: 0;
-    padding: 0;
-  }
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 
-  body {
-    font-family: Arial, Helvetica, sans-serif;
-    line-height: 1.4;
-  }
+body {
+  font-family: Arial, Helvetica, sans-serif;
+  line-height: 1.4;
+}
 
-  .btn {
-    display: inline-block;
-    border: none;
-    background: #555;
-    color: #fff;
-    padding: 7px 20px;
-    cursor: pointer;
-  }
+.btn {
+  display: inline-block;
+  border: none;
+  background: #555;
+  color: #fff;
+  padding: 7px 20px;
+  cursor: pointer;
+}
 
-  .btn:hover {
-    background: #666;
-  }
+.btn:hover {
+  background: #666;
+}
 </style>
